95f90b5374e2a58223174b7f96638dfb12cc660a
lanbow93-pi <the.doctor1501@gmail.com>
2023-09-25 07:41:16 -0500

    basic of python again

---
+++ lesson1.py
@@ -0,0 +1,21 @@
+# Lazy Playground Relearn
+x=7
+y=2
+z=x+y
+
+if (x<1):
+    print(z)
+else:
+    print("Hello")
+
+arrX = [5,4,3,2,1]
+arrX.insert(1, 9)
+print(arrX)
+
+complexArray = [[1,2,3],[4,5],[6,7,8,9]]
+
+
+for x in complexArray:
+    print(x)
+    
+print(complexArray[0][1])
